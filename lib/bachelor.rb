13a317cdcdc479fb91613c395d0f65f59535110e
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 21:12:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,3 +17,14 @@ def get_contestant_name(data, occupation)
   end
 end
 
+def count_contestants_by_hometown(data, hometown)
+  count = 0
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
+end	
