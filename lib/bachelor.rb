10dc812dcf5db35514ed0fbd03eafa5931900d29
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 21:10:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,7 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
-
-def get_contestant_name(data, occupation)
-  # code here
-end
-
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
-
-def get_occupation(data, hometown)
-  # code here
-end
-
-def get_average_age_for_season(data, season)
-  # code here
-end
+  # code here	  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(' ').first
+    end
+  end
+end	
