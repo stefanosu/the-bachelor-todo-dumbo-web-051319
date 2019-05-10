4a25fce8eca677aa507ad886093faebd1b29860e
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 21:10:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  # code here	  data[season].each do |contestant|
+  data[season].each do |contestant|
     if contestant["status"] == "Winner"
       return contestant["name"].split(' ').first
     end
